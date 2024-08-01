b0c63a74ea6765e3bce4c6ceafd4b2d1fdce5542
kanghyekyeong <ghgghg96@naver.com>
2024-08-01 10:21:12 +0900

    new

--- codes/08-data-structure/01-string.py
+++ codes/08-data-structure/01-string.py
@@ -1,105 +1,100 @@
-# find(x) x의 첫번째 위치를 반환. 없으면 -1을 반환
-# text = 'banana'
-# print(text.find('a')) # 1
-# print('banana'.find('z')) # -1
-# print('banana'.find('na')) # 2
+# find
+text = 'banana'
+print(text.find('a'))
+print(text.find('z'))
 
-# index(x) x의 첫번째 위치를 반환. 없으면 오류 발생
-# print(text.index('a')) # 1
-# print(text.index('z')) # ValueError: substring not found
+# index
+print(text.index('a'))
+# print(text.index('z'))
 
-# isupper(x)/islower(x) 문자열이 모두 대문자/소문자로 이루어져 있는지 확인
-# string1 = 'HELLO'
-# string2 = 'Hello'
-# print(string1.isupper()) # T
-# print(string2.isupper()) # F
-# print(string1.islower()) # F
-# print(string2.islower()) # F
+# isupper
+string1 = 'HELLO'
+string2 = 'Hello'
+print(string1.isupper())  # T
+print(string2.isupper())  # F
 
-# isalpha(x) 문자열이 알파벳으로만 이루어져 있는지 확인
-# string3 = 'Hello'
-# string4 = '123'
-# print(string3.isalpha()) # T
-# print(string4.isalpha()) # F
+# islower
+print(string1.islower())  # F
+print(string2.islower())  # F
 
-# replace(old, new[,count]) 바꿀 대상 글자를 새로운 글자로 바꿔서 반환
-# text = 'Hello, world!'
-# new_text = text.replace('world', 'Python')
-# print(new_text) #Hello, Python!
-
-# strip([chars]) 문자열의 시작과 끝에 있는 공백 혹은 지정한 문자를 제거
-# text = '  Hello, world!   '
-# new_text = text.strip()
-# print(new_text) # Hello, world!
+# isalpha
+string1 = 'Hello'
+string2 = '123heis98576ssh'
+print(string1.isalpha())  # T
+print(string2.isalpha())  # F
 
 
-# split(sep=None, maxsplit=-1) 지정한 문자를 구분자로 문자열을 분리하여 문자열의 리스트로 반환
-# text = 'Hello, world!'
-# words = text.split(',')
-# words2 = text.split()
-# print(words) # ['Hello', 'words!']
-# print(words) # ['Hello,', 'world!']
+# replace
+text = 'Hello, world! world world'
+new_text = text.replace('world', 'Python', 1)
+print(new_text)  # Hello, Python!
+
+# strip
+text = '  Hello, world!  '
+new_text = text.strip()
+print(new_text)
+
+# split
+text = 'Hello, world!'
+words = text.split(',')
+words2 = text.split()
+print(words)  # ['Hello', ' world!'] 
+print(words2)  # ['Hello,', 'world!'] 
 
 
-# join  iterable 의 문자열을 연결한 문자열을 반환
-# words = ['Hello', 'world!']
-# text = '-'.join(words)
-# print(text) #Hello-world!
+# join
+words = ['Hello', 'world!']
+new_text = '-'.join(words)
+print(new_text)  # Hello-world!
 
-# capitalize 첫번째단어를 대문자로 반환
-# text = 'heLLo, woRld!'
-# new_text1 = text.capitalize()
-# print(new_text1) #Hello, world!
+# capitalize
+text = 'heLLo, woRld!'
+new_text1 = text.capitalize()
+print(new_text1)  # Hello, world!
 
-# title 각 문자열의 첫번째 단어를 대문자로 반환
-# new_text2 = text.title()
-# print(new_text2) #Hello, World!
+# title
+new_text2 = text.title()
+print(new_text2)  # Hello, World!
 
-# upper 모두 대문자로 반환
-# new_text3 = text.upper()
-# print(new_text3) #HELLO, WORLD!
+# upper
+new_text3 = text.upper()
+print(new_text3)  # HELLO, WORLD!
 
-# lower 모두 소문자로 반환
-# new_text4 = text.lower()
-# print(new_text4) #hello, world!
+# lower
+new_text4 = text.lower()
+print(new_text4)  # hello, world!
 
-# swapcase 대문자<->소문자 변경 후 반환
-# new_text5 = text.swapcase()
-# print(new_text5) #HEllO, WOrLD!
+# swapcase
+new_text5 = text.swapcase()
+print(new_text5)  # HEllO, WOrLD!
 
-#메서드 이어서 사용하기(모든 메서드는 이어서 사용가능)
-# text = 'heLLo, woRld!'
-# new_text0 = text.swapcase().replace('l', 'z')
-# print(new_text0) #HEzzO, WOrLD!
-# new_text0 = text.swapcase().replace('L', 'z')
-# print(new_text0) #HEllO, WOrzD!
 
-# # 참고
-# # isdecimal() : 가장 엄격한 기준을 적용, 오직 일반적인 십진수 숫자(0-9)만 True로 인식
-# print("isdecimal() 메서드 예시:") #isdecimal() 메서드 예시:
-# print("'12345'.isdecimal():", '12345'.isdecimal()) #'12345'.isdecimal(): True
-# print("'123.45'.isdecimal():", '123.45'.isdecimal()) #'123.45'.isdecimal(): False
-# print("'-123'.isdecimal():", '-123'.isdecimal()) #'-123'.isdecimal(): False
-# print("'Ⅳ'.isdecimal():", 'Ⅳ'.isdecimal()) #'Ⅳ'.isdecimal(): False
-# print("'½'.isdecimal():", '½'.isdecimal()) #'½'.isdecimal(): False
-# print("'²'.isdecimal():", '²'.isdecimal()) #'²'.isdecimal(): False
-# print() #\n
+# 참고
+# isdecimal() : 가장 엄격한 기준을 적용, 오직 일반적인 십진수 숫자(0-9)만 True로 인식
+print("isdecimal() 메서드 예시:")
+print("'12345'.isdecimal():", '12345'.isdecimal())
+print("'123.45'.isdecimal():", '123.45'.isdecimal())
+print("'-123'.isdecimal():", '-123'.isdecimal())
+print("'Ⅳ'.isdecimal():", 'Ⅳ'.isdecimal())
+print("'½'.isdecimal():", '½'.isdecimal())
+print("'²'.isdecimal():", '²'.isdecimal())
+print()
 
-# # isdigit() : 일반 숫자뿐만 아니라 지수 표현(²)도 True로 인식
-# print("isdigit() 메서드 예시:") #isdigit() 메서드 예시:
-# print("'12345'.isdigit():", '12345'.isdigit()) #'12345'.isdigit(): True
-# print("'123.45'.isdigit():", '123.45'.isdigit()) #'123.45'.isdigit(): False
-# print("'-123'.isdigit():", '-123'.isdigit()) #'-123'.isdigit(): False
-# print("'Ⅳ'.isdigit():", 'Ⅳ'.isdigit()) #'Ⅳ'.isdigit(): False
-# print("'½'.isdigit():", '½'.isdigit()) #'½'.isdigit(): False
-# print("'²'.isdigit():", '²'.isdigit()) #'²'.isdigit(): True
-# print() #\n
+# isdigit() : 일반 숫자뿐만 아니라 지수 표현(²)도 True로 인식
+print("isdigit() 메서드 예시:")
+print("'12345'.isdigit():", '12345'.isdigit())
+print("'123.45'.isdigit():", '123.45'.isdigit())
+print("'-123'.isdigit():", '-123'.isdigit())
+print("'Ⅳ'.isdigit():", 'Ⅳ'.isdigit())
+print("'½'.isdigit():", '½'.isdigit())
+print("'²'.isdigit():", '²'.isdigit())
+print()
 
-# # isnumeric() : 일반 숫자, 로마 숫자, 분수, 지수 등 다양한 형태의 숫자 표현을 True로 인식
-# print("isnumeric() 메서드 예시:") #isnumeric() 메서드 예시:
-# print("'12345'.isnumeric():", '12345'.isnumeric()) #'12345'.isdigit(): True
-# print("'123.45'.isnumeric():", '123.45'.isnumeric()) #'123.45'.isdigit(): False
-# print("'-123'.isnumeric():", '-123'.isnumeric()) #'-123'.isdigit(): False
-# print("'Ⅳ'.isnumeric():", 'Ⅳ'.isnumeric()) #'Ⅳ'.isdigit(): True
-# print("'½'.isnumeric():", '½'.isnumeric()) #'½'.isdigit(): True
-# print("'²'.isnumeric():", '²'.isnumeric()) #'²'.isdigit(): True
+# isnumeric() : 일반 숫자, 로마 숫자, 분수, 지수 등 다양한 형태의 숫자 표현을 True로 인식
+print("isnumeric() 메서드 예시:")
+print("'12345'.isnumeric():", '12345'.isnumeric())
+print("'123.45'.isnumeric():", '123.45'.isnumeric())
+print("'-123'.isnumeric():", '-123'.isnumeric())
+print("'Ⅳ'.isnumeric():", 'Ⅳ'.isnumeric())
+print("'½'.isnumeric():", '½'.isnumeric())
+print("'²'.isnumeric():", '²'.isnumeric())
--- codes/08-data-structure/02-list.py
+++ codes/08-data-structure/02-list.py
@@ -1,63 +1,66 @@
-# 아래의 예시코드를 작성 가능해야함
+# append
+my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list)
+print(my_list.append(4))  # None
+
+# extend
+my_list = [1, 2, 3]
+my_list.extend([4, 5, 6])
+print(my_list)  # [1, 2, 3, 4, 5, 6]
+# my_list.extend(5)
+# print(my_list)
+my_list.extend([5, 6, 7])
+print(my_list)
+my_list.append([9, 9, 9])
+print(my_list)  # [1, 2, 3, 4, 5, 6, [9, 9, 9]]
+
+# insert
+my_list = [1, 2, 3]
+my_list.insert(1, 5)
+print(my_list)  # [1, 5, 2, 3]
+
+# remove
+my_list = [1, 2, 3, 2, 2, 2]
+my_list.remove(2)
+print(my_list)  # [1, 3, 2, 2, 2]
+
+# pop
+my_list = [1, 2, 3, 4, 5]
+item1 = my_list.pop()
+item2 = my_list.pop(0)
+
+print(item1)  # 5
+print(item2)  # 1
+print(my_list)  # [2, 3, 4]
 
 
-# append(x) 리스트 마지막에 항목 x를 추가
-my_list = ['a', 'b', 'c']
-my_list.append('d')
-print(my_list) #['a', 'b', 'c', 'd']
-print(my_list.append(4)) #None
-print(my_list[3]) #d
-print(my_list[4]) #4 print(my_list.append(4)) 에서 출력도 요청하지만 추가도 함
+# clear
+my_list = [1, 2, 3]
+my_list.clear()
+print(my_list)  # []
 
-# extend(iterable) 리스트에 다른 반복 가능한 객체의 모든 항목을 추가
-# my_list = [1, 2, 3]
-# my_list.extend([4,5,6])
-# print(my_list) #[1, 2, 3, 4, 5, 6]
+# index
+my_list = [1, 2, 3]
+index = my_list.index(2)
+print(index)  # 1
 
-# insert(i,x) 리스트의 지정한 인덱스 i 위치에 항목 x를 삽입(추가)
-# my_list = [1, 2, 3]
-# my_list.insert(1,5)
-# print(my_list) [1, 5, 2, 3]
+# count
+my_list = [1, 2, 2, 3, 3, 3]
+counting_number = my_list.count(3) 
+print(counting_number)  # 3
 
-# remove(x) 리스트에서 첫번째로 일치하는 항목 삭제
-# my_list = [1, 2, 3]
-# my_list.remove(2)
-# print(my_list) # [1, 3]
+# reverse
+my_list = [1, 3, 2, 8, 1, 9]
+my_list.reverse()
+print(my_list.reverse())  # None
+print(my_list)  # [9, 1, 8, 2, 3, 1]
 
-# pop(i) 리스트에서 지정한 인덱스의 항목을 제거하고 반환
-# 작성하지 않을 경우 마지막 항목을 제거
-# my_list = [1, 2, 3, 4, 5]
-# item1 = my_list.pop()
-# item2 = my_list.pop(0)
-# print(item1) # 5
-# print(item2) # 1
-# print(my_list) # [2, 3, 4]
-
-# clear() 리스트의 모든 항목을 삭제
-# my_list = [1, 2, 3]
-# my_list.clear()
-# print(my_list) # []
-
-# index(x) 리스트에서 첫번째로 일치하는 항목 x의 인덱스를 반환
-# my_list = [1, 2, 3]
-# index = my_list.index(2)
-# print(index) # 1
-
-# count(x) 리스트에서 항목 x의 개수를 반환
-# my_list = [1, 2, 2, 3, 3, 3]
-# count = my_list.count(3)
-# print(count) # 3
-
-# reverse() 리스트의 순서를 역순으로 변경(정렬 X)
-# my_list = [1, 3, 2, 8, 1, 9]
-# my_list.reverse()
-# print(my_list) #[9, 1, 8, 2, 3, 1]
-
-# sort() 원본 리스트를 오름차순으로 정렬
-# my_list = [3, 2, 100, 1]
-# my_list.sort() #오름차순
-# print(my_list) #[1, 2, 3, 100]
+# sort
+my_list = [3, 2, 100, 1]
+my_list.sort()
+print(my_list)  # [1, 2, 3, 100]
 
 # sort(내림차순 정렬)
-# my_list.sort(reverse=True)
-# print(my_list) #[100, 3, 2, 1]
+my_list.sort(reverse=True)
+print(my_list)  # [100, 3, 2, 1]
