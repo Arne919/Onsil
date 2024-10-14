d7db59b18c2979acda2083fe896445bce1c8c1b6
kanghyekyeong <ghgghg96@naver.com>
2024-10-14 15:49:37 +0900

    hw_3_4

---
+++ database/241011Fri/db_hw_3_4/libraries/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import Author, Book
+from .forms import BookForm
+
+# Create your views here.
+def index(request):
+    authors = Author.objects.all()
+    context = {
+        'authors': authors
+    }
+    return render(request, 'libraries/index.html', context)
+
+def detail(request, author_pk):
+    author = Author.objects.get(pk=author_pk)
+    books = author.book_set.all()
+    book_form = BookForm()
+    context = {
+        'author': author,
+        'book_form': book_form,
+        'books': books,
+    }
+    return render(request, 'libraries/detail.html', context)
+
+def books_create(request, author_pk):
+    author = Author.objects.get(pk=author_pk)
+    if request.method == 'POST':
+        book_form = BookForm(request.POST)
+        if book_form.is_valid():
+            book = book_form.save(commit=False)
+            book.author = author
+            book.save()
+        else:
+            context = {
+                'author': author,
+                'book_form': book_form
+            }
+            return render(request, 'articles/detail.html', context)
+        return redirect('libraries:detail', author.pk)
