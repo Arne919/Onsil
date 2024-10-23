1c858583177ef9bb1eddcdd8ed21c4b6296ce7a7
KangHyeKyeong <ghgghg96@naver.com>
2024-10-23 15:35:10 +0900

    ws_10_1

---
+++ django/241016Wed/django_ws_10_1/artists/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Artist
+from .serializers import ArtistSerializer
+
+# Create your views here.
+@api_view(['POST'])
+def artist_list_or_create(request):
+    if request.method == 'POST':
+        serializer = ArtistSerializer(data=request.POST)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
