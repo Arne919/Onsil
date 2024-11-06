019f5cf1e9c52cd3948c59abcc92152397568f9a
KangHyeKyeong <ghgghg96@naver.com>
2024-11-06 15:08:33 +0900

    ex_4_3

---
+++ vue/241106Wed/vue_ex_4_3/src/components/GalleryList.vue
@@ -0,0 +1,17 @@
+<template>
+	<main class="container py-3">
+		<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+			<GalleryListItem v-for="_ in numberOfItems" />
+		</div>
+	</main>
+</template>
+
+<script setup>
+import GalleryListItem from './GalleryListItem.vue';
+const numberOfItems = 4
+</script>
+
+
+<style scoped>
+
+</style>
