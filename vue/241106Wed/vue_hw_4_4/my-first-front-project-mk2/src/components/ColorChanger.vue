cfad5643d0bbf465202f37562a2cba982245830d
KangHyeKyeong <ghgghg96@naver.com>
2024-11-06 15:08:10 +0900

    hw_4_4

---
+++ vue/241106Wed/vue_hw_4_4/my-first-front-project-mk2/src/components/ColorChanger.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input v-model="colorClass" placeholder="red, blue, green 입력하시오." />
+        <p :class="colorClass">입력창에 올바른 색상 명을 입력하면 글자색이 바껴요.</p>
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        colorClass: ''
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .red {
+    color: red;
+  }
+  .blue {
+    color: blue;
+  }
+  .green {
+    color: green;
+  }
+  </style>
+  
