5215d068e7e13a2f0d8eab4e06d462bccb2d51a9
KangHyeKyeong <ghgghg96@naver.com>
2024-11-07 17:37:28 +0900

    ex_5_1

---
+++ vue/241107Thu/vue_ex_5_1/src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <li v-for="product in products" :key="product.id">
+      {{ product.name }} - {{ product.price }}원
+    </li>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  products: Array
+})
+</script>
+
+<style scoped>
+
+</style>
