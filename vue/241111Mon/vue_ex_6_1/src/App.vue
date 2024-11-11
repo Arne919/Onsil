3242655fa150173a409a6c01f0ef0c7050c4da47
KangHyeKyeong <ghgghg96@naver.com>
2024-11-11 17:03:54 +0900

    ex_6_1

---
+++ vue/241111Mon/vue_ex_6_1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{name: 'home'}">Home</RouterLink> |
+        <RouterLink :to="{name: 'about'}">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
