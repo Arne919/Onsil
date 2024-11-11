1232a7781d8d0e0335784793d807ea7dbda31d47
KangHyeKyeong <ghgghg96@naver.com>
2024-11-11 17:03:58 +0900

    ex_6_2

---
+++ vue/241111Mon/vue_ex_6_2/src/views/UserView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>User Profile</h1>
+    <p>Username: {{ username }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  username: String
+})
+</script>
+
+<style scoped>
+</style>
