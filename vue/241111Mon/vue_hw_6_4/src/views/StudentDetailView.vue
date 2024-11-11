c77cd325c2cd7c999f6437b7aa6c3741a9ddcc18
KangHyeKyeong <ghgghg96@naver.com>
2024-11-11 17:03:47 +0900

    hw_6_4

---
+++ vue/241111Mon/vue_hw_6_4/src/views/StudentDetailView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>학생 상세 정보 페이지</h1>
+    <p>{{ name }} 학생의 상세 정보입니다.</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  name: String, // 라우트에서 전달받은 name 파라미터를 props로 받아옵니다
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/241111Mon/vue_hw_6_4/src/views/StudentView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>학생 상세 정보 목록 입니다.</h1>
+    <ul>
+      <li v-for="student in students" :key="student.name">
+        <button @click="gotoStudentDetail(student.name)">{{ student.name }} 학생 정보 보기</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import {useRouter} from 'vue-router'
+const students = [
+  { name: '김하나'},
+  { name: '김두리'},
+  { name: '김서이'},
+]
+const router = useRouter()
+function gotoStudentDetail(name) {
+  router.push({ name: 'studentDetail', params: {name} })
+  
+}
+</script>
+
+<style scoped>
+
+</style>
