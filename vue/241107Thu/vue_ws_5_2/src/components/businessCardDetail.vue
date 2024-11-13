a308b8762d271f5de1546b67fba36e3bbb9e2ccc
KangHyeKyeong <ghgghg96@naver.com>
2024-11-13 16:29:43 +0900

    ws_5_2

---
+++ vue/241107Thu/vue_ws_5_2/src/components/businessCardDetail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="business-card-detail">
+    <h3>이름 : {{ businessCard.name }}</h3>
+    <p>직함 : {{ businessCard.title }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  businessCard: Object
+})
+</script>
+
+<style>
+.business-card-detail {
+  width: 40%;
+  margin: 20px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  background-color: #f9f9f9;
+}
+</style>
