a7947e6974b7d852394b009ddab4730ceaec4b8f
KangHyeKyeong <ghgghg96@naver.com>
2024-11-17 22:44:01 +0900

    vue_ws_6_3

---
+++ vue/241111Mon/vue_ws_6_4/src/components/QuizDetail.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="quiz-item">
+    <h5 class="quiz-question">{{ quiz.pk }}번 문제. {{ quiz.question }}</h5>
+    <label class="answer-label" :for="'answer' + quiz.pk">정답 입력</label>
+    <input 
+      type="text" 
+      :id="'answer' + quiz.pk" 
+      class="answer-input"
+      v-model="userAnswer"
+      @keyup.enter="submitAnswer(quiz)"
+    >
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue';
+defineProps({
+  quiz: Object
+})
+
+const router = useRouter()
+const submitAnswer = (quiz) => {
+  console.log(quiz)
+  router.push(
+    {
+      name: 'answer', 
+      params:
+        { 
+          pk: quiz.pk, 
+          answer: quiz.answer
+        }, 
+      query: 
+        {
+          userAnswer: userAnswer.value 
+        }
+    })
+}
+
+const userAnswer = ref('');
+
+</script>
+
+<style  scoped>
+
+.quiz-item {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 20px 0;
+  background-color: #f9f9f9;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+}
+
+.quiz-question {
+  font-size: 18px;
+  margin: 0;
+}
+
+.answer-label {
+  display: block;
+  font-size: 16px;
+  margin-top: 15px;
+}
+
+.answer-input {
+  width: calc(100% - 24px);
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  margin-top: 8px;
+}
+
+</style>
---
+++ vue/241111Mon/vue_ws_6_4/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '/src/views/HomeView.vue';
+import QuizView from '/src/views/QuizView.vue';
+import AnswerView from '/src/views/AnswerView.vue';
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/quiz',
+      name: 'quiz',
+      component: QuizView,
+    },
+    {
+      path: '/answer/:pk/:answer',
+      name: 'answer',
+      component: AnswerView,
+    },
+  ]
+})
+
+export default router
