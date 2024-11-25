8c7427bb809a4ada3646c715d6f394a0ce54e9c1
KangHyeKyeong <ghgghg96@naver.com>
2024-11-25 22:46:29 +0900

    vue_ws_7_3

---
+++ vue/241112Tue/vue_ws_7_3/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="shop-container">
+    <h1 class="shop-title">쑈핑몰</h1>
+    <p class="shop-description">쇼핑몰 설명</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.shop-container {
+  text-align: center;
+  padding: 20px;
+}
+
+.shop-title {
+  font-size: 36px;
+  margin-bottom: 10px;
+}
+
+.shop-description {
+  font-size: 18px;
+  line-height: 1.5;
+}
+</style>
