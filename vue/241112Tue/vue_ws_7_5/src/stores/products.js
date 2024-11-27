4edf627f493915804543760f26378cd934ebd3a7
KangHyeKyeong <ghgghg96@naver.com>
2024-11-27 22:47:00 +0900

    vue_ws_7_5

---
+++ vue/241112Tue/vue_ws_7_5/src/stores/products.js
@@ -0,0 +1,45 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useProductListStore = defineStore('productList', () => {
+  const productList = ref([
+    {
+      name: '상품 1',
+      imagePath: 'src/assets/product1.png',
+      price: 10000,
+      isFavorite: false
+    },
+    {
+      name: '상품 2',
+      imagePath: 'src/assets/product2.png',
+      price: 20000,
+      isFavorite: false
+    },
+    {
+      name: '상품 3',
+      imagePath: 'src/assets/product3.png',
+      price: 30000,
+      isFavorite: false
+    },
+    {
+      name: '상품 4',
+      imagePath: 'src/assets/product4.png',
+      price: 40000,
+      isFavorite: false
+    },
+  ])
+
+  const updateIsFavorite = function (name) {
+    productList.value = productList.value.map((product) => {
+      if (product.name === name) {
+        product.isFavorite = !product.isFavorite
+      }
+      return product
+    })
+  }
+
+  const isFavoriteList = computed (() => {
+    return productList.value.filter((product) => product.isFavorite)
+  })
+  return { productList, updateIsFavorite, isFavoriteList }
+}, { persist: true })
---
+++ vue/241112Tue/vue_ws_7_5/src/views/FavoriteView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="product-list-page">
+    <h1 class="page-title">장바구니 페이지</h1>
+    <div class="product-card-list">
+      <ProductDetail 
+        v-for="product in store.isFavoriteList"
+        :key="product.name"
+        :product="product"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useProductListStore } from '@/stores/products.js'
+import ProductDetail from '../components/ProductDetail.vue';
+const store = useProductListStore()
+</script>
+
+<style scoped>
+.product-list-page {
+  padding: 20px;
+}
+
+.page-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.product-card-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
