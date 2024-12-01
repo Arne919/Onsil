91919f45f149ce9a90ed2963403fd54e911b5002
KangHyeKyeong <ghgghg96@naver.com>
2024-12-01 22:49:35 +0900

    vue_ex_8_2

---
+++ vue/241113Wed/vue_ex_8_2/answer1/vue-project/src/components/TodoListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ todo.work}}</p>
+    <p>{{ todo.content}}</p>
+    <p>{{ todo.is_completed}}</p>
+    <p>{{ todo.created_at}}</p>
+  </div>
+  <hr/>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  todo: Object
+})
+</script>
