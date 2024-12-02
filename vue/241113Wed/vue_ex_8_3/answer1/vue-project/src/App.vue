8a578be45a342df217201b9689177f3f4d7a6b0f
KangHyeKyeong <ghgghg96@naver.com>
2024-12-02 22:49:49 +0900

    vue_ex_8_3

---
+++ vue/241113Wed/vue_ex_8_3/answer1/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">할 일 목록</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
